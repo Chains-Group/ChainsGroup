60b16b37d118cc43ced6eb75e42eecea99e3c552
DaveChains <david.i.cadenas@gmail.com>
2013-09-19 12:48:06 -0430

    se agrego packetes y se borraron classes de prueba main

--- MainDrive.java
+++
@@ -1,5 +0,0 @@
-class MainDrive {
-	public static void main(String [] args) {
-	    System.out.println("primer codigo guardado");
-	}
-}
---
+++ src/com/zoo/app/Animal.java
@@ -0,0 +1,7 @@
+package com.zoo.app;
+
+public abstract class Animal {
+    Animal() {
+    	System.out.println("Works !!! ");
+    }
+}
