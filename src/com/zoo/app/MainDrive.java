0c05cac8bfebac29d8cac477f785abba95e54e2c
DaveChains <david.i.cadenas@gmail.com>
2013-09-25 12:21:39 -0430

    Jugando con Objetos y polimorfismo avanzado, Casting de Objectos luego de pasar por un Arry list con parametros <Object> volviendo al estado natural del obeto

--- src/com/zoo/app/MainDrive.java
+++ src/com/zoo/app/MainDrive.java
@@ -2,20 +2,8 @@ package com.zoo.app;
 import java.util.*;
 class MainDrive {
 	public static void main(String [] args) {
-	    System.out.println("primer codigo guardado");
-	    AnimalList al = new AnimalList();
-        Dog d = new Dog();
-        Cat c = new Cat();
-	    al.add(d);
-	    al.add(c);
-        
-
-
-        Dog d3 = new Dog();
-		Dog d4 = (Dog)getObject(d3);
-		System.out.println("Casting " + d4.toString()+ "Casting");
-
-
+	    
+	    go();
 
 
 	}
@@ -23,5 +11,32 @@ class MainDrive {
 
 	public static Object getObject(Object o){
 		return o;
+	}
+
+	public static void go(){
+        Dog d = new Dog();
+		Cat c = new Cat();
+
+		Object d1 = getObject(d);
+		Object c1 = getObject(c);
+
+		ArrayList<Object> l1 = new ArrayList<Object>();
+		
+         
+		l1.add(d1);
+        l1.add(c1);
+
+        Object o1 = l1.get(0);
+        Object o2 = l1.get(1);
+        System.out.println(o1.toString() + " - " + o2.toString() );
+
+        Dog d2 = (Dog)o1;
+        Cat c2 = (Cat)o2;
+
+        
+        System.out.println("Dog & Cats Again = " );
+        d2.makeNoise();
+        c2.makeNoise();
+
 	}
 }
